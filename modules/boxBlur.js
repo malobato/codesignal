e7b0566cc3e6588f1db08f37588961e721c9a36e
Miguel Ángel Lobato Álvarez <malobato@coderszone.com>
2023-12-22 19:15:35 +0100

    Removed boxBlur module test

--- modules/boxBlur.js
+++ modules/boxBlur.js
@@ -14,12 +14,3 @@ export const solution = (image) => {
   }
   return result
 }
-
-const image = [
-  [7, 4, 0, 1],
-  [5, 6, 2, 2],
-  [6, 10, 7, 8],
-  [1, 4, 2, 0]
-]
-
-console.log(solution(image))
