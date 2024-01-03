677bcbb35791d6e519213f2d0a61f4733738c4da
Miguel Ángel Lobato Álvarez <malobato@coderszone.com>
2024-01-03 18:23:59 +0100

    Optimized circleOfNumbers solution

--- modules/circleOfNumbers.js
+++ modules/circleOfNumbers.js
@@ -1,4 +1,3 @@
 export const solution = (n, firstNumber) => {
-  const position = Math.floor(n / 2) + firstNumber
-  return (position < n) ? position : (position % n)
+  return ((n / 2) + firstNumber) % n
 }
