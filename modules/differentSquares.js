6e6fe61acf219bc560e36681f4c96716b8efe405
Miguel Ángel Lobato Álvarez <malobato@coderszone.com>
2024-02-01 13:04:55 +0100

    Fixed previous push

--- modules/differentSquares.js
+++ modules/differentSquares.js
@@ -18,3 +18,22 @@ export const solution = (matrix) => {
   }
   return arrays.length
 }
+
+const betterSolution = (matrix) => {
+  const rows = matrix.length
+  const columns = matrix[0].length
+  if (rows === 1 || columns === 1) {
+    return 0
+  }
+  const arrays = new Set()
+  for (let row = 0; row < rows - 1; row++) {
+    for (let col = 0; col < columns - 1; col++) {
+      const current = JSON.stringify([
+        matrix[row][col], matrix[row][col + 1],
+        matrix[row + 1][col], matrix[row + 1][col + 1]
+      ])
+      arrays.add(current)
+    }
+  }
+  return arrays.size
+}
