fb71aee65e560ff2f792f3ab5928ae93e8f41d02
Miguel Ángel Lobato Álvarez <malobato@coderszone.com>
2024-02-10 21:14:18 +0100

    Fixed seatsInTheater test

--- __tests__/theCore/seatsInTheater.spec.js
+++ __tests__/theCore/seatsInTheater.spec.js
@@ -1,5 +1,5 @@
 import { test, expect } from 'vitest'
-import * as seatsInTheater from './modules/'
+import * as seatsInTheater from './modules/theCore/seatsInTheater'
 
 test('Seats in theater working case', () => {
   expect(seatsInTheater.solution(16, 11, 5, 3)).toEqual(96)
